1bfb965a3807cd22f04960e27962fa201191629b
Bdnature <84246065+Quddos@users.noreply.github.com>
2023-07-16 00:25:57 -0700

    using textTheme font changing the size and color

--- lib/main.dart
+++ lib/main.dart
@@ -69,6 +69,19 @@ class BigCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text(pair.asLowerCase);
+    final theme = Theme.of(context);
+
+    final style = theme.textTheme.displayMedium!.copyWith(
+      color: theme.colorScheme.onPrimary,
+    );
+
+    return Card(
+      color: theme.colorScheme.primary,
+      child: Padding(
+        padding: const EdgeInsets.all(20),
+        // child: Text(pair.asLowerCase),
+        child: Text(pair.asLowerCase, style: style),
+      ),
+    );
   }
 }
